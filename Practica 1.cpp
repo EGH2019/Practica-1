51dbc53aefe5b8abbf8899c026ee56dc55e2e2b2
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2019-09-06 12:12:08 -0500

    version 1

---
+++ Practica 1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    printf("inserte los numeros a sumar");
+    scanf("%d%d", &a,&b);
+    c= a+b;
+    printf("%d", c);
+    return 0;
+
+
+}
